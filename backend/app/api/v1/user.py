c2a6f302eef04879e005e9e36a37cdc901db8969
Vanilia <yuyan124@hotmail.com>
2023-01-26 20:34:29 +0800

    :art: 调整代码结构

--- backend/app/api/v1/user.py
+++ backend/app/api/v1/user.py
@@ -4,29 +4,30 @@ import pydantic
 from app import schemas
 from faker import Faker
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 
-def create_fake_user():
+def create_fake_user(id: int):
     faker = Faker(locals="zh-CN")
     return {
         "email": faker.email(),
         "phone": faker.phone_number(),
         "is_superuser": True,
         "nickname": faker.name(),
-        "id": 1,
+        "id": id,
         "status": 1,
         "create_time": faker.date_time().strftime("%Y-%m-%d %H:%M:%S"),
     }
 
 
-@router.get("/", response_class=schemas.User)
-def read_users() -> Any:
-    return create_fake_user()
+@router.get("/", response_class=schemas.User, response_model_exclude_unset=True)
+def read_users() -> JSONResponse:
+    return JSONResponse(content=jsonable_encoder(create_fake_user(1)))
 
 
 @router.get("/{user_id}", response_class=schemas.User)
-async def get_user(user_id: int) -> Any:
-
-    return create_fake_user()
+async def get_user(user_id: int) -> JSONResponse:
+    return JSONResponse(content=jsonable_encoder(create_fake_user(user_id)))
--- backend/app/main.py
+++ backend/app/main.py
@@ -5,4 +5,4 @@ app = carete_app()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=10000)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=10000, debug=True)
