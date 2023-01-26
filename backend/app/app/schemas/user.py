baea2dad171ccd1908bd65eeb9ffaeb15f61afe0
Vanilia <yuyan124@hotmail.com>
2023-01-26 15:45:14 +0800

    :new: User Schemas and Router

---
+++ backend/app/app/schemas/user.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: Optional[EmailStr] = None
+    phone: Optional[str] = None
+    is_superuser: bool = False
+    nickname: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserDbBase(UserBase):
+    id: Optional[int] = None
+    status: Optional[int] = None
+    create_time: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class User(UserDbBase):
+    pass
+
+
+class UserInDb(UserDbBase):
+    hashed_password: str
