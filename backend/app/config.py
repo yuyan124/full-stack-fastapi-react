8f0fa45b6a3965b6d944ef7b27fac692e4ef4df8
Vanilia <yuyan124@hotmail.com>
2023-01-27 16:18:04 +0800

    :wrench: postgresql

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,11 @@
+import os
 import secrets
-import typing
+from typing import Any, Dict, Optional
 
 import pydantic
+from dotenv.main import load_dotenv
+
+load_dotenv()
 
 
 class Setting(pydantic.BaseSettings):
@@ -11,11 +15,19 @@ class Setting(pydantic.BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
 
     # PROJECT_NAME: str
-    # POSTGRESQL_SERVER: str
-    # POSTGRESQL_USER: str
-    # POSTGRESQL_PASSWORD: str
-    # POSTGRESQL_DB: str
-    SQLALCHEMY_DATABASE_URI: typing.Optional[pydantic.PostgresDsn] = None
+    POSTGRESQL_SERVER: str = os.environ["POSTGRES_SERVER"]
+    POSTGRESQL_USER: str = os.environ["POSTGRES_USER"]
+    POSTGRESQL_PASSWORD: str = os.environ["POSTGRES_PASSWORD"]
+    POSTGRESQL_DB: str = os.environ["POSTGRES_DB"]
+    SQLALCHEMY_DATABASE_URI: Optional[
+        pydantic.PostgresDsn
+    ] = pydantic.PostgresDsn.build(
+        scheme="postgresql",
+        user=os.environ["POSTGRES_USER"],
+        password=os.environ["POSTGRES_PASSWORD"],
+        host=os.environ["POSTGRES_SERVER"],
+        path=f"/{os.environ['POSTGRES_DB'] or ''}",
+    )
 
     class Config:
         case_sensitive = True
