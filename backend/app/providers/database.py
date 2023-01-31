ddab4822700b093dedd474ec226b55c42fa5e57d
Vanilia <yuyan124@hotmail.com>
2023-01-31 15:24:45 +0800

    :art: 增加db日志打印，使用延迟初始化

--- backend/app/providers/database.py
+++ backend/app/providers/database.py
@@ -4,18 +4,18 @@ from app.config import setting
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_async_engine(setting.ASYNC_SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+# @brief:
+#   @param: echo, 打印执行日志
+#   @param: future: 使用2.0行特性, engine延迟初始化，直到第一次请求真正连接
+engine = create_async_engine(
+    setting.ASYNC_SQLALCHEMY_DATABASE_URI,
+    pool_pre_ping=True,
+    future=True,
+    # echo=True,
+)
 SessionLocal: Callable[..., AsyncSession] = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
 )
-
-
-# def get_db():
-#     try:
-#         db = SessionLocal()
-#         yield db
-#     finally:
-#         db.close()
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
