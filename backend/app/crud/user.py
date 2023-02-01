a817a760e66f49fc1828aa5c6c7120b5e51f9377
Vanilia <yuyan124@hotmail.com>
2023-02-01 16:09:42 +0800

    :art: 移除无效注释

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -9,20 +9,6 @@ from sqlalchemy import select, text, update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-# async def create_user(db: AsyncSession, user: UserCreate) -> User:
-#     db_user = User(
-#         email=user.email,
-#         password=user.password,
-#         nickname=user.nickname,
-#         create_time=int(datetime.now().timestamp()),
-#     )
-#     async with db.begin():
-#         db.add(db_user)
-#         await db.commit()
-#         # await db.refresh(db_user)
-#         db.expunge(db_user)
-#         return db_user
-
 
 class CrudUser(CrudBase[User, UserCreate, UserUpdate]):
     async def create(self, db: AsyncSession, *, user_in: UserCreate) -> Any:
