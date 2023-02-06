dc76b155642b7505a3526adcff54a0a30f75cedf
Vanilia <yuyan124@hotmail.com>
2023-02-06 21:08:13 +0800

    :art: add login router

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from app.api.v1 import user
+from app.api.v1 import login
 from fastapi import APIRouter
 
 api_router = APIRouter()
+api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
