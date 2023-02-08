c251e3c95177a6d7d85799d7a81fbd676640a9cd
Vanilia <yuyan124@hotmail.com>
2023-02-08 16:20:33 +0800

    :pencil: 注释

--- backend/app/api/v1/user.py
+++ backend/app/api/v1/user.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pydantic
 from app import crud, schemas
-from app.errors import UserExist, UserNotExist
+from app.errors import PermissionDenied, UserExist, UserNotExist
 from app.providers.database import get_db
 from app.response.user import UserListResponse, UserResponse
 from faker import Faker
@@ -32,7 +32,20 @@ async def create_user(
     *,
     db: AsyncSession = Depends(get_db),
     user_in: schemas.UserCreate,
-) -> Any:
+) -> JSONResponse:
+    """
+    创建新用户
+
+    @param:
+        user_in: 待创建新用户的信息
+        db: 数据库session. Defaults to Depends(get_db).
+
+    @raise:
+        UserExist: 用户存在，
+
+    @return:
+        JSONResponse: 成功返回创建用户的信息，失败返回错误码及错误信息。
+    """
     user = await crud.user.get_by_email(db, email=user_in.email)
     if user:
         raise UserExist
@@ -45,6 +58,17 @@ async def create_user(
 async def get_users(
     db: AsyncSession = Depends(get_db), skip: int = 0, limit: int = 100
 ) -> JSONResponse:
+    """
+    批量读取用户。
+
+    @param:
+        db: 数据库session. Defaults to Depends(get_db).
+        skip: 从第skip之后读取. Defaults to 0.
+        limit: 读取用户的数量. Defaults to 100.
+
+    @return:
+        JSONResponse: 成功返回用户信息。
+    """
     users = await crud.user.get_multi(db, skip=skip, limit=limit)
     r = UserListResponse(code=0, success=True, data=users)
     return JSONResponse(content=jsonable_encoder(r))
@@ -52,6 +76,19 @@ async def get_users(
 
 @router.get("/{user_id}", response_model=UserResponse)
 async def get_user(user_id: int, db: AsyncSession = Depends(get_db)) -> JSONResponse:
+    """
+    通过user_id获取特定用户信息。
+
+    @param:
+        user_id: 用户id
+        db: 数据库session. Defaults to Depends(get_db).
+
+    @raise:
+        UserNotExist: 用户不存在
+
+    @return:
+        JSONResponse: 成功返回用户信息，失败返回错误码及错误信息。
+    """
     user = await crud.user.get(db, id=user_id)
     if not user:
         raise UserNotExist
@@ -63,9 +100,23 @@ async def get_user(user_id: int, db: AsyncSession = Depends(get_db)) -> JSONResp
 async def update_user(
     *, db: AsyncSession = Depends(get_db), user_id: int, user_in: schemas.UserUpdate
 ) -> JSONResponse:
-    # user = await crud.user.get(db, id=user_id)
-    # if not user:
-    # raise UserNotExist
-    user = await crud.user.update(db, id=user_id, obj_in=user_in)
+    """
+    通过user_id更新用户信息
+
+    @param:
+        user_id: 用户id
+        user_in: 待更新的用户信息
+        db: 数据库session. Defaults to Depends(get_db).
+
+    @raise:
+        UserNotExist: 用户不存在
+
+    @return:
+        JSONResponse: 成功返回用户信息，失败返回错误码及错误信息。
+    """
+    user = await crud.user.get(db, id=user_id)
+    if not user:
+        raise UserNotExist
+    user = await crud.user.update(db, db_obj=user, obj_in=user_in)
     r = UserResponse(code=0, success=True, data=user)
     return JSONResponse(content=jsonable_encoder(r))
