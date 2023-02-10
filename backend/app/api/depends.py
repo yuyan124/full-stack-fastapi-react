2e3deff96985c55367ce0d67a41a73dbf0ead08e
Vanilia <yuyan124@hotmail.com>
2023-02-10 16:29:12 +0800

    :new: 激活用户和管理员验证

--- backend/app/api/depends.py
+++ backend/app/api/depends.py
@@ -1,14 +1,19 @@
+from app import crud, models, schemas
+from app.config import setting
+from app.errors import (
+    AuthFailed,
+    ExpiredToken,
+    InactiveUser,
+    PermissionDenied,
+    UserNotExist,
+)
+from app.providers.database import get_db
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from jose.exceptions import ExpiredSignatureError, JWTError
 from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from app import crud, models, schemas
-from app.config import setting
-from app.errors import AuthFailed, ExpiredToken, UserNotExist
-from app.providers.database import get_db
 
 oauth2 = OAuth2PasswordBearer(tokenUrl=f"{setting.API_PREFIX}/login/token")
 
@@ -28,3 +33,19 @@ async def get_current_user(
     if not user:
         raise UserNotExist
     return user
+
+
+async def get_current_active_user(
+    current_user: models.User = Depends(get_current_user),
+) -> models.User:
+    if not await crud.user.is_active(current_user):
+        raise InactiveUser
+    return current_user
+
+
+async def get_current_superuser(
+    current_user: models.User = Depends(get_current_user),
+) -> models.User:
+    if not await crud.user.is_superuser(current_user):
+        raise PermissionDenied
+    return current_user
