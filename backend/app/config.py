4ba2ca6cb8b8e4f2c81e60384f01e1d7f01ffd69
Vanilia <yuyan124@hotmail.com>
2023-02-13 21:42:07 +0800

    :new: email config and email sender with simple template

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, List, Optional
 
 import pydantic
 from dotenv.main import load_dotenv
+from pydantic.networks import EmailStr
 
 load_dotenv()
 
@@ -56,7 +57,17 @@ class Setting(pydantic.BaseSettings):
 
     LOG_DIRECTORY = "logs/"
 
+    # ------------------------------------------
+    # email
+    # ------------------------------------------
     EMAIL_ENABLED_CONFIRM: bool = True
+    EMAIL_STMP_HOST: Optional[str] = os.environ["EMAIL_STMP_HOST"]
+    EMAIL_STMP_PORT: Optional[int] = os.environ["EMAIL_STMP_PORT"]
+    EMAIL_STMP_USERNAME: Optional[str] = os.environ["EMAIL_STMP_USERNAME"]
+    EMAIL_STMP_PASSWORD: Optional[str] = os.environ["EMAIL_STMP_PASSWORD"]
+    EMAIL_FROM_EMAIL: Optional[EmailStr] = os.environ["EMAIL_FROM_EMAIL"]
+    EMAIL_FROM_NAME: Optional[str] = os.environ["EMAIL_FROM_NAME"]
+    EMAIL_TEMPLATE_DIR: Optional[str] = os.environ["EMAIL_TEMPLATE_DIR"]
 
     class Config:
         case_sensitive = True
---
+++ backend/app/providers/email.py
@@ -0,0 +1,34 @@
+from typing import List, Optional, Union
+
+from pydantic.networks import EmailStr
+from redmail import EmailSender
+
+from app.config import setting
+from app.providers.log import logger
+from app.schemas import UserCreate
+
+e = EmailSender(
+    host=setting.EMAIL_STMP_HOST,
+    port=setting.EMAIL_STMP_PORT,
+    username=setting.EMAIL_STMP_USERNAME,
+    password=setting.EMAIL_STMP_PASSWORD,
+)
+e.sender = setting.EMAIL_FROM_EMAIL
+e.set_template_paths(html=setting.EMAIL_TEMPLATE_DIR)
+
+
+def send_email(
+    receviers: Union[List[EmailStr], EmailStr],
+    subject: str,
+    html_template: Optional[str] = None,
+) -> None:
+    msg = e.send(subject=subject, receivers=receviers, html_template=html_template)
+    logger.debug(msg)
+
+
+def send_new_account_email(
+    receviers: Union[List[EmailStr], EmailStr], user: UserCreate
+) -> None:
+
+    subject = f"{setting.PROJECT_NAME} - New Account for user {user.email}"
+    send_email(receviers, subject, "new_account.html")
